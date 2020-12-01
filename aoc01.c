3093b9856f013543fe6398a33a6278e75cb2c3f4
alex wennerberg <alex@alexwennerberg.com>
2020-11-30 22:38:32 -0800

    remove unneccessary imports

--- aoc01.c
+++ aoc01.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define TARGET 2020
 #define MAX_LENGTH 200
 
 int main() {
-  // input items assumed 0 < input < TARGET  and len(items) < MAX_LENGTH
   int entrycount[TARGET];
   int entries[MAX_LENGTH];
-  // initialize to 0
   for (int i = 0; i < TARGET; i ++) {
     entrycount[i] = 0;
   }
@@ -17,6 +13,7 @@ int main() {
   int c;
   while(scanf("%d", &entry) == 1) {
     if(entrycount[TARGET - entry] != 0) {
+      // Part 1
       printf("%d\n", entry * (TARGET - entry));
     } 
     entries[c] = entry;
@@ -24,11 +21,11 @@ int main() {
     c++;
   }
 
-  // brute force part 2
   for (int i = 0; i < c ; i ++) {
     for (int j = i; j < c; j++) {
       for (int k = j; k < c; k ++) {
         if ((entries[i] + entries[j] + entries[k]) == TARGET) {
+          // Part 2
           printf("%d\n", entries[i] * entries[j] * entries[k]);
           return 0;
         }
