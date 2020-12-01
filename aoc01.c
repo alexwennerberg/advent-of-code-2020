3ac5e6b43c97086cb6d890e3074d3bd39c05b559
alex wennerberg <alex@alexwennerberg.com>
2020-11-30 21:47:14 -0800

    Part 1 solution

--- aoc01.c
+++ aoc01.c
@@ -3,6 +3,19 @@
 #include <string.h>
 
 int main() {
-  printf("Hello, World!");
-  return 0;
+  // input items assumed 0 < input < 2020 
+  int entries[2020];
+  // initialize to 0
+  for (int i = 0; i < 2020; i ++) {
+    entries[i] = 0;
+  }
+  int entry;
+  while(scanf("%d", &entry) == 1) {
+    if(entries[2020 - entry] == 1) {
+      printf("%d", entry * (2020 - entry));
+      return 0;
+    } 
+    entries[entry] = 1;
+  }
+  return 1;
 }
