89767a391aecb1ec7fff4dd94035cc6d1c2865e2
alex wennerberg <alex@alexwennerberg.com>
2020-12-01 23:36:19 -0800

    Day 2 in C

---
+++ aoc02.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MIN 0
+#define MAX 1
+#define CHAR 2
+#define PW 3
+
+int main() {
+  char input[128];
+  int valid_one = 0;
+  int valid_two = 0;
+  while(fgets(input, 128, stdin)) {
+    char c;
+    int mode = 0; 
+    int min = 0;
+    int max = 0;
+    char req; 
+    int count = 0;
+
+    int pp = 1;
+    int valid_pos = 0;
+    for(int i = 0; 1; i++ ) {
+      c = input[i];
+      if(c == '-'){
+        mode++;
+      } else if(c == ' '){
+        mode++;
+      } else if(c == ':'){
+        continue;
+      } else  if(mode == MIN){
+          min = min * 10 + c - '0';
+        } else if(mode == MAX){
+          max = max * 10 + c - '0';
+        } else if(mode == CHAR){
+          req = c;
+        } else if(mode == PW){
+          if(c == req){
+            count ++;
+            if(pp == min || pp == max){
+              valid_pos++;
+            }
+          }
+          if(c == '\0') {
+            break;
+          }
+          pp++;
+        }
+    }
+    if(count <= max && count >= min){
+      valid_one++;
+    }
+    if(valid_pos == 1) {
+      valid_two++;
+    }
+  }
+  // part 1
+  printf("%d\n", valid_one);
+  // part 2
+  printf("%d\n", valid_two);
+  return 0;
+}
