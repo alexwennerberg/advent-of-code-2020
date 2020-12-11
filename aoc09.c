d1827a97f1f2bc5b53ebd1da060df7a241f0dd6b
alex wennerberg <alex@alexwennerberg.com>
2020-12-10 20:42:47 -0800

    day 9 part 2

--- aoc09.c
+++ aoc09.c
@@ -9,6 +9,7 @@ int main() {
   int i = 0;
   // generate program
   char* input[32];
+  int partone;
   while(fgets(input, 32, stdin)) { // \0 and \n i think
     int value = atoi(input);
     xmas[i] = value;
@@ -25,6 +26,25 @@ int main() {
       }
       if(!sums){
         printf("%d\n", value);
+        partone = value;
+      }
+    }
+  }
+  for(i = 0; i <= 1024; i++) {
+    int sum = 0;
+    int j;
+    int max = 0;
+    int min = 100000000; //  idk
+    for(j = i; sum < partone; j++) {
+      sum += xmas[j];
+      if (xmas[j] < min) {
+        min = xmas[j];
+      }
+      if (xmas[j] > max) {
+        max = xmas[j];
+      }
+      if(sum == partone){
+        printf("%d\n", min + max);
         return;
       }
     }
