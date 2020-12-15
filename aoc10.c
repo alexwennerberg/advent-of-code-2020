70565ad807dbf56a2bf3b400faf84a42947bb090
alex wennerberg <alex@alexwennerberg.com>
2020-12-14 19:48:17 -0800

    Part 2

--- aoc10.c
+++ aoc10.c
@@ -8,6 +8,18 @@ int cmpfunc (const void * a, const void * b) {
      return ( *(int*)a - *(int*)b );
 }
 
+
+unsigned long long int paths[256];
+
+unsigned long long int get_value(int v) {
+  if (v < 0) {
+    return 0;
+  } else if (v == 0) {
+    return 1;
+  }else {
+    return paths[v];
+  }
+}
 int main() {
   char input[8];
   int i = 0;
@@ -23,6 +35,20 @@ int main() {
     last = joltages[i];
   }
   freq[3]++; // device voltage
-  printf("%d", freq[1] * freq[3]);
+  // Part 1
+  printf("%d\n", freq[1] * freq[3]);
+  // Part 2
+  int device_voltage = joltages[INPUT_LENGTH] + 3;
+  int possible_arrangements = 1;
 
+  int value;
+  for(i=0; i<=INPUT_LENGTH; i++) {
+    if(i == INPUT_LENGTH) {
+      value = value + 3;
+    } else {
+      value = joltages[i];
+    }
+    paths[value] = get_value(value-2) + get_value(value-1) + get_value(value-3);
+  }
+  printf("%lld\n", paths[value]);
 }
