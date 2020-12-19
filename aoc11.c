ba0cd3f1daefdc868a7558bfd5f3a94ef7923ec0
alex wennerberg <alex@alexwennerberg.com>
2020-12-19 15:34:15 -0800

    day 11 part 2

--- aoc11.c
+++ aoc11.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
+/* This one is messy */
+
 #define EMPTY 'L'
 #define OCCUPIED '#'
 #define FLOOR '.'
 
-/* #define WIDTH 90 */
-/* #define HEIGHT 91 */
-#define WIDTH 10
-#define HEIGHT 10
+#define WIDTH 90
+#define HEIGHT 91
+/* #define WIDTH 10 */
+/* #define HEIGHT 10 */
 
-char seats[HEIGHT][WIDTH];
+typedef char SEATGRID[HEIGHT][WIDTH];
+SEATGRID seats;
+
+
+void copy_seats(SEATGRID from, SEATGRID to) {
+  int x,y;
+  for(y=0;y<HEIGHT;y++) {
+    for(x=0;x<WIDTH;x++) {
+      to[y][x] = from[y][x];
+    }
+  }
+}
 
 int swap_seats(int mode) { // returns n occupied
   char newseats[HEIGHT][WIDTH];
@@ -24,22 +37,36 @@ int swap_seats(int mode) { // returns n occupied
       int n;
       int occupied_adjacent = 0;
       for(n=0; n < 8; n++) {
-        int xdiff =  neighbors[n][0];
-        int ydiff =  neighbors[n][1];
-        if(
-            x+xdiff >= 0 
-            && x+xdiff < WIDTH 
-            && y+ydiff >= 0 
-            && y+ydiff < HEIGHT 
-            && seats[y+ydiff][x+xdiff] == OCCUPIED
-            ) {
-          occupied_adjacent++;
+        // For part two, we keep searching in this direction
+        int vision = 1;
+        while (1) {
+          int xdiff =  neighbors[n][0] * vision;
+          int ydiff =  neighbors[n][1] * vision;
+          if(
+              x+xdiff >= 0 
+              && x+xdiff < WIDTH 
+              && y+ydiff >= 0 
+              && y+ydiff < HEIGHT 
+            ){
+            if (seats[y+ydiff][x+xdiff] == OCCUPIED ) {
+              occupied_adjacent++;
+              break;
+            } else if (seats[y+ydiff][x+xdiff] == EMPTY ) {
+              break;
+            }
+          } else {
+            break;
+          }
+          if(mode == 1) {
+            break;
+          }
+          vision++;
         }
       }
       char new_state;
       if(state == EMPTY && occupied_adjacent == 0) {
         new_state = OCCUPIED;
-      } else  if(state == OCCUPIED && occupied_adjacent >= 4) {
+      } else  if(state == OCCUPIED && occupied_adjacent >= 4 + mode - 1) {
         new_state = EMPTY;
       } else {
         new_state = state;
@@ -51,15 +78,11 @@ int swap_seats(int mode) { // returns n occupied
     }
   }
   // copy over
-  for(y=0;y<HEIGHT;y++) {
-    for(x=0;x<WIDTH;x++) {
-      seats[y][x] = newseats[y][x];
-    }
-  }
+  copy_seats(newseats, seats);
   return occupied;
 }
 
-int print_seats() {
+void print_seats() {
   int x, y;
   for(y=0;y<HEIGHT;y++) {
     for(x=0;x<WIDTH;x++) {
@@ -87,16 +110,21 @@ int main () {
   }
   int last = -1;
   int occ;
-  int i;
+  SEATGRID temp;
+  copy_seats(seats, temp);
   while(occ != last ) {
     last = occ;
     // specify part number
     occ = swap_seats(1);
-    print_seats();
-    printf("%d\n", occ);
   }
-  /* while(swap_seats(2) != last) { */
-  /*   printf("%d\n", occ); */
+  printf("%d\n", occ);
+  // Reset
+  occ = 0;
+  copy_seats(temp, seats);
+  while(occ != last) {
+    last = occ;
+    occ = swap_seats(2);
     /* print_seats(); */
-  /* } */
+  }
+  printf("%d\n", occ);
 }
